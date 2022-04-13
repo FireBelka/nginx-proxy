83d874dde9fcb99a3e182e1cc5c4f7239c84522c
Danila Belko <danilabelko@gmail.com>
2022-04-13 13:10:13 +0300

    working set

--- shutdown.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-az login --service-principal -u "..." -p "..." --tenant "..."
-az acr webhook delete -n 'nginxProxyBackendWebhook' -r '<registry>'
-az acr webhook delete -n 'nginxProxynginxProxyServiceWebhookWebhook' -r '<registry>'
-terraform destroy -auto-approve
--- startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#az acr login -n testRegK8s
-az login --service-principal -u "..." -p "..." --tenant "..."
-terraform apply -auto-approve
-VAR1=$(az webapp deployment container config --enable-cd true -n '<app-name>' -g '<RG>' --slot 'staging' | jq '.CI_CD_URL' | tr -d '"')
-az acr webhook create -n 'nginxProxyBackendWebhook' -r '<Registry>' --uri $VAR1 --actions push --scope nginx-backend:latest
-az acr webhook create -n 'nginxProxyServiceWebhook' -r '<Registry>' --uri $VAR1 --actions push --scope nginx-proxy:latest
